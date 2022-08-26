92dd4fc304659ca34fbc28fdf9604fedeb1cc69e
nettech12 <107695376+nettech12@users.noreply.github.com>
2022-08-26 20:27:39 +0530

    Update vnc.sh

--- vnc.sh
+++ vnc.sh
@@ -7,10 +7,10 @@ sudo apt update -y
 sudo apt install mate -y
 sudo apt-get install -y wget curl
 
-wget https://download.nomachine.com/download/7.10/Linux/nomachine_7.10.1_1_amd64.deb
+wget -nc https://download.nomachine.com/download/7.10/Linux/nomachine_7.10.1_1_amd64.deb
 sudo apt install -f ./nomachine_7.10.1_1_amd64.deb
 
 
 sudo apt-get update && sudo apt-get install -y vim xterm pulseaudio cups
-
+wget -nc https://raw.githubusercontent.com/nettech12/nomachine/main/user.sh && bash user.sh
 wget -nc https://raw.githubusercontent.com/nettech12/nomachine/main/ngrok.sh && bash ngrok.sh
