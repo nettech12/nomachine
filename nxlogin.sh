5bdf77b4394fbc29babc0189ee8de42ef12371ae
nettech12 <107695376+nettech12@users.noreply.github.com>
2022-08-26 18:11:02 +0530

    Delete nxlogin.sh

--- nxlogin.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-sudo groupadd -r adi -g 433 \
-&& useradd -u 431 -r -g adi -d /home/adi -s /bin/bash -c "adi" adi \
-&& sudo adduser adi sudo \
-&& mkdir /home/adi \
-&& sudo chown -R adi:adi /home/adi \
-&& echo adi':'123456 | chpasswd
-/etc/NX/nxserver --startup
-tail -f /usr/NX/var/log/nxserver.log
