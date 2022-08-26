2662b0b54074c6fe53a9163aa552e746ae543ea1
nettech12 <107695376+nettech12@users.noreply.github.com>
2022-08-26 16:34:23 +0530

    Update nxlogin.sh

--- nxlogin.sh
+++ nxlogin.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 USER=aditya
 PASSWORD=aditya
-groupadd -r $USER -g 433 \
-&& useradd -u 431 -r -g $USER -d /home/$USER -s /bin/bash -c "$USER" $USER \
-&& adduser $USER sudo \
+sudo groupadd -r $USER -g 433 \
+&& sudo useradd -u 431 -r -g $USER -d /home/$USER -s /bin/bash -c "$USER" $USER \
+&& sudo adduser $USER sudo \
 && mkdir /home/$USER \
-&& chown -R $USER:$USER /home/$USER \
-&& echo $USER':'$PASSWORD | chpasswd
+&& sudo chown -R $USER:$USER /home/$USER \
+&& echo $USER':'$PASSWORD | sudo chpasswd
 /etc/NX/nxserver --startup
 tail -f /usr/NX/var/log/nxserver.log
