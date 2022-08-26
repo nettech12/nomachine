d0cf02c230a0ee6b4adc5e8714ba0b61f231d512
nettech12 <107695376+nettech12@users.noreply.github.com>
2022-08-26 16:32:18 +0530

    Update nx.sh

--- nx.sh
+++ nx.sh
@@ -14,6 +14,6 @@ sudo apt install -f ./nomachine_7.10.1_1_amd64.deb &&
 echo "d833ad52f92e5b3cc30c96f12686d97f *nomachine_7.10.1_1_amd64.deb"
 
 sudo sed -i "s|#EnableClipboard both|EnableClipboard both |g" /usr/NX/etc/server.cfg
-wget -nc https://raw.githubusercontent.com/nettech12/nomachine/main/nxlogin.sh && chmod +x nxlogin.sh && ./nxlogin.sh
+wget -nc https://raw.githubusercontent.com/nettech12/nomachine/main/nxlogin.sh && bash nxlogin.sh
 wget -nc https://raw.githubusercontent.com/nettech12/nomachine/main/tcp.sh && bash tcp.sh 
 rm -rf /var/lib/apt/lists/*
