e23d7f6548c61b16b25725ed19b8ae22e39d0500
nettech12 <107695376+nettech12@users.noreply.github.com>
2022-08-26 16:14:50 +0530

    Add files via upload

---
+++ nx.sh
@@ -0,0 +1,17 @@
+#!bin/sh
+echo "========"
+echo "UPDATING"
+echo "========"
+sudo apt-get update && sudo apt-get install -y vim xterm pulseaudio cups && clear
+echo "========"
+echo "FILES"
+echo "========"
+sudo apt update -y
+sudo apt install mate -y
+sudo apt -y install wget
+wget https://download.nomachine.com/download/7.10/Linux/nomachine_7.10.1_1_amd64.deb
+sudo apt install -f ./nomachine_7.10.1_1_amd64.deb && 
+echo "d833ad52f92e5b3cc30c96f12686d97f *nomachine_7.10.1_1_amd64.deb"
+ && sudo sed -i "s|#EnableClipboard both|EnableClipboard both |g" /usr/NX/etc/server.cfg
+wget -nc https://raw.githubusercontent.com/nettech12/nomachine/main/nxlogin.sh && chmod +x nxlogin.sh && ./nxlogin.sh 
+rm -rf /var/lib/apt/lists/*
