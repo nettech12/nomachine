5ae032660c9c1d33469e059cd869c768232cd372
nettech12 <107695376+nettech12@users.noreply.github.com>
2022-08-26 18:12:19 +0530

    Add files via upload

---
+++ nx.sh
@@ -0,0 +1,19 @@
+#!bin/sh
+DEBIAN_FRONTEND=noninteractive
+NOMACHINE_PACKAGE_NAME=nomachine_5.2.11_1_amd64.deb
+NOMACHINE_MD5=d697e5a565507d522380c94d2f295d07
+apt-get update && apt-get install -y vim xterm pulseaudio cups curl
+sudo apt-get -y update && sudo apt-get -y dist-upgrade && sudo apt-get clean
+curl -fSL "http://download.nomachine.com/download/5.2/Linux/${NOMACHINE_PACKAGE_NAME}" -o nomachine.deb \
+&& echo "${NOMACHINE_MD5} *nomachine.deb" | md5sum -c - \
+&& dpkg -i nomachine.deb \
+&& groupadd -r nomachine -g 433 \
+&& useradd -u 431 -r -g nomachine -d /home/nomachine -s /bin/bash -c "NoMachine" nomachine \
+&& adduser nomachine sudo \
+&& mkdir /home/nomachine \
+&& chown -R nomachine:nomachine /home/nomachine \
+&& echo 'nomachine:nomachine' | chpasswd
+apt-get install -y python3-software-properties software-properties-common wget xdg-utils libpango1.0-0 fonts-liberation
+add-apt-repository ppa:webupd8team/tor-browser
+# RUN apt-get update -y && apt-get install -y tor firefox libreoffice htop nano git vim tor-browser
+apt-get update -y && apt-get install -y tor firefox libreoffice htop nano git vim tor-browser
---
+++ nxserver.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+/etc/NX/nxserver --startup
+tail -f /usr/NX/var/log/nxserver.log
+CRP=2DnrN2wZbKO5ZUR0yF0GUhYtPs1_6txMMmS4zRSb3EB3DycJN
+wget -nc -O ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
+unzip ngrok.zip > /dev/null 2>&1
+chmod +x ngrok
+./ngrok authtoken $CRP
+./ngrok tcp --region ap 4000 &>/dev/null &
+sleep 1
+curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
+seq 1 43200 | while read i; do echo -en "\r Running .     $i s /43200 s";sleep 0.1;echo -en "\r Running ..    $i s /43200 s";sleep 0.1;echo -en "\r Running ...   $i s /43200 s";sleep 0.1;echo -en "\r Running ....  $i s /43200 s";sleep 0.1;echo -en "\r Running ..... $i s /43200 s";sleep 0.1;echo -en "\r Running     . $i s /43200 s";sleep 0.1;echo -en "\r Running  .... $i s /43200 s";sleep 0.1;echo -en "\r Running   ... $i s /43200 s";sleep 0.1;echo -en "\r Running    .. $i s /43200 s";sleep 0.1;echo -en "\r Running     . $i s /43200 s";sleep 0.1; done
