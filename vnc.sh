66023ecd560ceeafdaf177e459439954643f3161
nettech12 <107695376+nettech12@users.noreply.github.com>
2022-08-27 13:20:35 +0530

    Update vnc.sh

--- vnc.sh
+++ vnc.sh
@@ -6,12 +6,9 @@ NOMACHINE_MD5=d833ad52f92e5b3cc30c96f12686d97f
 sudo apt update -y
 sudo apt install mate -y
 sudo apt-get install -y wget curl
-
 wget -nc https://download.nomachine.com/download/7.10/Linux/nomachine_7.10.1_1_amd64.deb
 sudo apt install -f ./nomachine_7.10.1_1_amd64.deb
-
-wget -nc https://raw.githubusercontent.com/nettech12/nomachine/main/app.py && python3 app.sh
+sudo apt install firefox-esr -y
 sudo apt-get update && sudo apt-get install -y vim xterm pulseaudio cups
 wget -nc https://raw.githubusercontent.com/nettech12/nomachine/main/user.sh && bash user.sh
 wget -nc https://raw.githubusercontent.com/nettech12/nomachine/main/ngrok.sh && bash ngrok.sh
- 
