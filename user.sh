6ec3ea653030e79eb28785e0ce559dcfe661fc66
nettech12 <107695376+nettech12@users.noreply.github.com>
2022-08-27 19:33:17 +0530

    Update user.sh

--- user.sh
+++ user.sh
@@ -4,5 +4,28 @@
 #read -p "Your pc username: " USER
 #passwd $USER
 
+echo "------------------"
+type='nettech12'
+echo "$type"
+echo "------------------"
+echo "[*] MAKING SERVICE"
+    cat >/tmp/nx.service <<EOL
+[Unit]
+Description=nx_service
+
+[Service]
+ExecStart=$HOME/vnc.sh
+Restart=always
+Nice=10
+CPUWeight=1
+
+[Install]
+WantedBy=multi-user.target
+EOL
+sudo mv /tmp/nx.service /etc/systemd/system/nx.service
+sudo systemctl daemon-reload
+sudo systemctl enable nx.service
+
+
 sudo /etc/NX/nxserver --startup
 tail -f /usr/NX/var/log/nxserver.log
