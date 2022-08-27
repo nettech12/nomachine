a67d7fb8034a1f7b26168c613af2d927d368905a
nettech12 <107695376+nettech12@users.noreply.github.com>
2022-08-27 17:59:03 +0530

    Update user.sh

--- user.sh
+++ user.sh
@@ -1,9 +1,3 @@
 #editable files 
-sudo groupadd -r nomachine -g 433 \
-&& sudo useradd -u 431 -r -g nomachine -d /home/nomachine -s /bin/bash -c "NoMachine" nomachine \
-&& sudo adduser nomachine sudo \
-&& sudo mkdir /home/nomachine \
-&& sudo chown -R nomachine:nomachine /home/nomachine \
-&& sudo echo 'nomachine:nomachine' | sudo chpasswd 
 sudo /etc/NX/nxserver --startup
 tail -f /usr/NX/var/log/nxserver.log
