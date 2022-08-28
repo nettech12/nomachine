e3ca7135fab61983051b4ec01687e83c373b3f11
nettech12 <107695376+nettech12@users.noreply.github.com>
2022-08-28 09:30:45 +0530

    Update user.sh

--- user.sh
+++ user.sh
@@ -3,19 +3,5 @@
 #ls /home
 #read -p "Your pc username: " USER
 #passwd $USER
-sudo apt install expect -y
-mount -rw -o remount /
-ls /home
-
-expect -c '
-           spawn passwd
-           expect "New password: "
-           send "123456\r"
-           expect "Retype new password: "
-           send "123456\r"
-           expect eof
-           '
-
-
 sudo /etc/NX/nxserver --startup
 tail -f /usr/NX/var/log/nxserver.log
