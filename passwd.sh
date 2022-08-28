8e89c765719e002902ceee455e373e7194ceddd5
nettech12 <107695376+nettech12@users.noreply.github.com>
2022-08-28 09:11:52 +0530

    Update passwd.sh

--- passwd.sh
+++ passwd.sh
@@ -6,7 +6,7 @@ expect -c '
            spawn passwd
            expect "New password: "
            send "123456\r"
-           expect "Retype new password"
+           expect "Retype new password: "
            send "123456\r"
            expect eof
            '
